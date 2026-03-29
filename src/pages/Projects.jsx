89eedc98b82be65ba8baf6eb522024f8673b88da
Pranav Sonawane <prsaso999@gmail.com>
2026-03-29 23:27:44 +0530

    v3.1 gray vertical lines

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import BlinkLink from '../components/BlinkLink';
 
 export default function Projects() {
-  return (
-    <main className="w-full min-h-screen pt-32 pb-40 flex flex-col items-center">
-      {/* Horizontal Sub-Navigation */}
-      <div className="w-full flex justify-between px-8 max-w-screen-2xl mx-auto">
-        <BlinkLink href="/projects">{'{ Wedding }'}</BlinkLink>
-        <BlinkLink href="/projects">{'{ Engagement }'}</BlinkLink>
-        <BlinkLink href="/projects">{'{ Birthday }'}</BlinkLink>
-        <BlinkLink href="/projects">{'{ Maternity }'}</BlinkLink>
-        <BlinkLink href="/projects">{'{ Other }'}</BlinkLink>
-      </div>
+  const categories = [
+    '{ Wedding }', 
+    '{ Engagement }', 
+    '{ Birthday }', 
+    '{ Maternity }', 
+    '{ Other }'
+  ];
 
-      {/* 
-        The rest of the actual projects interface goes here.
-        For now, this space is preserved for the next implementation steps!
-      */}
-      <div className="flex-1 flex items-center justify-center">
-        <h2 className="text-4xl px-8 text-neutral-300 font-sans tracking-wide">Select a category</h2>
+  return (
+    <main className="w-full min-h-screen pt-32 pb-32 flex flex-col items-center">
+      <div className="w-full flex-1 flex justify-between px-8 max-w-screen-2xl mx-auto items-stretch">
+        {categories.map((cat) => (
+          <div key={cat} className="flex flex-col items-center group">
+            <BlinkLink href="/projects">{cat}</BlinkLink>
+            {/* The vertical gray line */}
+            <div className="w-[1px] flex-grow bg-gray-200 mt-6 min-h-[40vh] transition-colors duration-500 group-hover:bg-gray-400"></div>
+          </div>
+        ))}
       </div>
     </main>
   );
