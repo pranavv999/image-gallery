b3daac76d06cb677f56e0ee9d2a9daf028e29044
Pranav Sonawane <prsaso999@gmail.com>
2026-03-30 21:37:16 +0530

    will add link for home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,24 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full flex items-center justify-between px-8 py-6 z-50 bg-transparent font-sans">
-      <div 
-        className="cursor-pointer"
-        onMouseEnter={() => setLogoSrc('/shutter-click.png')}
-        onMouseLeave={() => setLogoSrc('/shutter-open.png')}
-      >
-        <img 
-          src={logoSrc} 
-          alt="Shutter Logo" 
-          className="w-8 h-8 object-contain transition-transform duration-300" 
-        />
-      </div>
+      <a href='/'>
+        <div
+          className="cursor-pointer"
+          onMouseEnter={() => setLogoSrc('/shutter-click.png')}
+          onMouseLeave={() => setLogoSrc('/shutter-open.png')}
+        >
+          <img
+            src={logoSrc}
+            alt="Shutter Logo"
+            className="w-8 h-8 object-contain transition-transform duration-300"
+          />
+        </div>
+      </a>
 
       <nav className="flex space-x-8 items-center">
-        <ScrambleLink href="/about">{'{ About }'}</ScrambleLink>
-        <ScrambleLink href="/projects">{'{ Projects }'}</ScrambleLink>
-        <ScrambleLink href="/contacts">{'{ Contacts }'}</ScrambleLink>
+        <ScrambleLink href="/about">{'{ ABOUT }'}</ScrambleLink>
+        <ScrambleLink href="/projects">{'{ PROJECTS }'}</ScrambleLink>
+        <ScrambleLink href="/contacts">{'{ CONTACTS }'}</ScrambleLink>
       </nav>
     </header>
   );
